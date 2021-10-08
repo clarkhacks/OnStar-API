f58f223214fc2070dfe6c7cf2f89c579dd660e56
Glitch (glitch-hello-node) <none>
2021-10-08 18:17:52 +0000

    🐴🎟 Checkpoint ./server.js:550756/331

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-import OnStar from "onstarjs";
+const OnStar = require("onstarjs");
 function runIt() {
 
 const config = {
   deviceId: "e81b6ad4-2860-11ec-9621-0242ac130002",
-  vin: "",
-  username: "foo@bar.com",
-  password: "p@ssw0rd",
-  onStarPin: "1234",
+  vin: process.env.SECRET,
+  username: process.env.USER,
+  password: process.env.PASS,
+  onStarPin: process.env.PIN,
 
   // Optional
-  checkRequestStatus: true, // When false, requests are complete when 'In Progress' (Much faster).
+  checkRequestStatus: false, // When false, requests are complete when 'In Progress' (Much faster).
   requestPollingIntervalSeconds: 6, // When checkRequestStatus is true, this is how often status check requests will be made
   requestPollingTimeoutSeconds: 60, // When checkRequestStatus is true, this is when requests while polling are considered timed out
 };
 
 const onStar = OnStar.create(config);
-
+console.log("made it here");
 onStar
   .alert({
-    action: ["Flash"],
+    action: ["Honk"],
   })
   .then(() => {
-    onStar.start();
+    onStar.start
+console.log("made it here now");
+  
   })
   .catch(e => console.log(e));
 }
+
+// runIt();
