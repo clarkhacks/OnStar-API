e92c73640b5d131f2ce0fbe8cdbf049ab8930e79
Glitch (glitch-hello-node) <none>
2021-10-09 22:00:58 +0000

    🍳😄 Checkpoint ./server.js:550756/13991 ./package.json:550756/1238

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const OnStar = require("onstarjs");
-function runIt() {
-
+var express = require("express");
+var app = express();
 const config = {
   deviceId: "e81b6ad4-2860-11ec-9621-0242ac130002",
   vin: process.env.SECRET,
@@ -11,21 +11,37 @@ const config = {
   // Optional
   checkRequestStatus: false, // When false, requests are complete when 'In Progress' (Much faster).
   requestPollingIntervalSeconds: 6, // When checkRequestStatus is true, this is how often status check requests will be made
-  requestPollingTimeoutSeconds: 60, // When checkRequestStatus is true, this is when requests while polling are considered timed out
+  requestPollingTimeoutSeconds: 60 // When checkRequestStatus is true, this is when requests while polling are considered timed out
 };
 
-const onStar = OnStar.create(config);
-console.log("made it here");
-onStar
-  .alert({
-    action: ["Honk"],
-  })
-  .then(() => {
-    onStar.start
-console.log("made it here now");
-  
-  })
-  .catch(e => console.log(e));
-}
+var onStar = OnStar.create(config);
 
-// runIt();
+app.get("/honk", function(req, res) {
+  console.log("made it here");
+  onStar
+    .alert({
+      action: ["Honk"]
+    })
+    .then(() => {
+      onStar.start;
+      console.log("made it here now");
+    })
+    .catch(e => console.log(e));
+  res.send("Hello World");
+});
+
+app.get("/stop", function(req, res) {
+  console.log("made it here");
+  onStar
+    .alert({
+      action: ["Honk"]
+    })
+    .then(() => {
+      onStar.Cancelstart;
+      console.log("made it here now");
+    })
+    .catch(e => console.log(e));
+  res.send("Hello World");
+});
+
+app.listen(3000);
